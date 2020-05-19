62cf94d9826e508ad667367ffb60531cfa47680d
yuliia-h <yuliiagusak@gmail.com>
2020-05-19 12:23:55 +0300

    Add test not work

--- interfaces/library.go
+++ interfaces/library.go
@@ -3,52 +3,66 @@ package interfaces
 import (
 	"bytes"
 	"encoding/base64"
-	"github.com/disintegration/imaging"
+	"image"
 	"projectServis/user_cases"
+
+	"github.com/disintegration/imaging"
 )
 
 type LibraryImages struct {
+	DecodeString   func(string) ([]byte, error)
+	Decode         func([]byte) (image.Image, error)
+	Resize100      func(image.Image) image.Image
+	Encode         func(*bytes.Buffer, image.Image, imaging.Format) error
+	EncodeToString func(src []byte) string
 }
 
 func NewLibraryImages() *LibraryImages {
-	return &LibraryImages{}
+	return &LibraryImages{
+		DecodeString: func(s string) ([]byte, error) {
+			return base64.StdEncoding.DecodeString(s)
+		},
+		Decode: func(s []byte) (image.Image, error) {
+			return imaging.Decode(bytes.NewReader(s))
+		},
+		Resize100: func(img image.Image) image.Image {
+			return imaging.Resize(img, 100, 0, imaging.Lanczos)
+		},
+		Encode: func(s *bytes.Buffer, img image.Image, format imaging.Format) error {
+			return imaging.Encode(s, img, imaging.PNG)
+		},
+		EncodeToString: func(src []byte) string {
+			return base64.StdEncoding.EncodeToString(src)
+		},
+	}
 }
 
 func (l LibraryImages) ResizeImageLibrary(image user_cases.Image) (user_cases.Image, error) {
 
 	//Декодируем base64 в байты
-	outPngData, err := base64.StdEncoding.DecodeString(image.Buffer)
+	outPngData, err := l.DecodeString(image.Buffer)
 	if err != nil {
 
 		return user_cases.Image{}, err
 	}
+
 	//преобразовать байты в структуру image.Image
-
-	im, err := imaging.Decode(bytes.NewReader(outPngData))
+	im, err := l.Decode(outPngData)
 	if err != nil {
 		return user_cases.Image{}, err
 	}
 
-	//var src = imaging.Resize(im, image.Width, image.Height, imaging.Lanczos)
-	var src = imaging.Resize(im, 100, 0, imaging.Lanczos)
+	var src = l.Resize100(im)
 
 	var buff bytes.Buffer
-	err = imaging.Encode(&buff, src, imaging.PNG)
+	err = l.Encode(&buff, src, imaging.PNG)
 	if err != nil {
 		return user_cases.Image{}, err
 	}
 
-	image.Buffer = base64.StdEncoding.EncodeToString(buff.Bytes())
+	image.Buffer = l.EncodeToString(buff.Bytes())
 	image.Height = (100 * image.Height) / image.Width
 	image.Width = 100
-
-	//f, err := os.Create("a.png")
-	//if err != nil {
-	//	return user_cases.Image{}, err
-	//}
-	//
-	//f.Write(buff.Bytes())
-	//f.Close()
 
 	return image, nil
 }
